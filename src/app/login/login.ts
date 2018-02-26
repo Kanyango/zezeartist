52d07e94c8bb8384bd330e068246cec8de9d92a8
Kanyango <kariukikanyango@gmail.com>
2018-02-26 10:19:06 +0300

    Update login.ts

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,36 @@
-import { Component , OnInit} from '@angular/core';
+
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { Headers, Http } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'login-component',
   templateUrl: 'login.html'
 })
-export class LoginComponent { }
+export class LoginComponent 
+{
+  public fbUrl = 'https://zezeapp.herokuapp.com/auth/facebook/';
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    constructor(private http: Http,
+            private router: Router,
+            private route: ActivatedRoute){}
+    
+    fbStrategy()
+    {
+       this.http.get(this.fbUrl)
+            .toPromise()
+            .then(res => {
+                        console.log(res.json());
+                        this.router.navigate(['../release']);
+
+             })
+            .catch(this.handleError)
+    }
+    private handleError(error: any): Promise<any>
+     {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+    }
+ 
+}

d29750685b2315f4fb62e58bc98f411161851083
Kanyango <kariukikanyango@gmail.com>
2018-02-26 09:51:05 +0300

    Update login.ts

---
+++ src/app/login/login.ts
@@ -0,0 +1,9 @@
+import { Component , OnInit} from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+@Component({
+  selector: 'login-component',
+  templateUrl: 'login.html'
+})
+export class LoginComponent { }

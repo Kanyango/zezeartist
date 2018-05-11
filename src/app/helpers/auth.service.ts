a361859fc5bece38deb91c8ed59fc51bccc83510
Kanyango <kariukikanyango@gmail.com>
2018-05-11 08:32:27 +0300

    Update auth.service.ts

--- src/app/helpers/auth.service.ts
+++ src/app/helpers/auth.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Http, Headers } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
  
 @Injectable()
 export class AuthenticationService {
-    constructor(private http: HttpClient) { }
+    constructor(private http: Http) { }
     
     private loginUrl = 'https://zezeafrica.herokuapp.com/login';
   
